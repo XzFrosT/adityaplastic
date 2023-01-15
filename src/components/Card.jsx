4422e26cd38cd9a6afb26eaaee11c70b0db006ba
XzFrosT <mschodeihsan@gmail.com>
2023-01-15 21:23:19 +0700

    up

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({src}) => {
 	return (
 	  <div className="text-center">
 	  	<div className="px-2">
-	  		<img src={src} width="100%" height="200vh"/>
+	  		<img src={src} alt="" width="100%" height="200vh"/>
 	  	</div>
 	  </div>
 	)
